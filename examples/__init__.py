2e9f2e76d13eb9cc3e30388dd2d1c8f3d93baf4f
YL <yl@YLs-MacBook-Pro.local>
2026-03-22 16:35:26 -0400

    project file

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,9 +1,4 @@
 """
 DKGP Examples
 
-Run examples:
-    python -m dkgp.examples.regression_example
-    python -m dkgp.examples.classification_example
-    python -m dkgp.examples.bayesian_optimization_example
-    python -m dkgp.examples.feature_extractor_comparison
 """
